b8b3af58e429f785e91dfb965c54b85012425272
JacoboGarcesO <jacobogarcesoquendo@gmail.com>
2022-02-13 21:52:19 -0500

    Changes

--- src/api/routes/locations.routes.ts
+++ src/api/routes/locations.routes.ts
@@ -1,18 +1,16 @@
 import { Application, Router } from 'express';
-import { JwtController } from '../controllers/jwt.controller';
 import { LocationsController } from '../controllers/locations.controller';
 
 export class LocationsRouter {
   private app: Application;
   private controller: LocationsController = new LocationsController();
-  private jwtController: JwtController = new JwtController();
 
   constructor(app: Application) { this.app = app; }
 
   init() {
     const router = Router();
     this.app.use('/api/locations', router);
-    router.get('/', this.jwtController.validateToken ,this.controller.getStates);
-    router.get('/:stateId', this.jwtController.validateToken ,this.controller.getCitiesByState);
+    router.get('/',this.controller.getStates);
+    router.get('/:stateId',this.controller.getCitiesByState);
   }
 }
