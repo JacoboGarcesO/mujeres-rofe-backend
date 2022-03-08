80fbd9686a0a331b968c6afd8bf7f29692d85c6b
JacoboGarcesO <jacobogarcesoquendo@gmail.com>
2022-03-08 16:46:37 -0500

    hola

--- src/collections/notices.collection.ts
+++ src/collections/notices.collection.ts
@@ -1,4 +1,4 @@
-import { model, Schema, SchemaTypes } from 'mongoose';
+import { model, Schema } from 'mongoose';
 import { mediaSchema } from './image.collection';
 
 const linkSchema = new Schema({
@@ -7,17 +7,16 @@ const linkSchema = new Schema({
 });
 
 const noticeSchema = new Schema({
-  formId: { type: Schema.Types.ObjectId, ref: 'FormRequests', required: [true, 'FormId is required'] },
   title: { type: String, required: [true, 'Notice title is required'] },
   order: { type: String, required: [true, 'Notice order is required'] },
   channel: { type: String, required: [true, 'Notice channel is required'] },
   showUsersList: { type: Boolean, default: false },
   isLink: { type: Boolean, default: false },
   url: { type: String },
+  formId: { type: Schema.Types.ObjectId, ref: 'FormRequests' },
   description: { type: String },
   icon: mediaSchema,
   content: mediaSchema,
-  form: SchemaTypes.ObjectId, ref: 'FormRequests',
   links: [linkSchema],
 }, { collection: 'Notices' });
 
