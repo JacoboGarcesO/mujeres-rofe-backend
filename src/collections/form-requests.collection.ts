d185c9b8a08686218953529f9330d36db0fd648a
JacoboGarcesO <jacobogarcesoquendo@gmail.com>
2022-06-05 22:19:31 -0500

    Form's description was added

--- src/collections/form-requests.collection.ts
+++ src/collections/form-requests.collection.ts
@@ -13,9 +13,10 @@ const fieldSchema = new Schema({
 
 
 const formRequestsSchema = new Schema({
-  title: { type: String, required: [true, 'Foorm title is required'] },
-  template: { type: String, required: [true, 'Foorm template is required'] },
-  subject: { type: String, required: [true, 'Foorm subject is required'] },
+  title: { type: String, required: [true, 'Form title is required'] },
+  description: { type: String, required: [true, 'Form description is required'] },
+  template: { type: String, required: [true, 'Form template is required'] },
+  subject: { type: String, required: [true, 'Form subject is required'] },
   fields: [fieldSchema],
   channel: { type: String },
 }, { collection: 'FormRequests' });
