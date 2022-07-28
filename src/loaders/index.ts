6cd3affbc80d1705b325eff3de79e75177e50f2c
JacoboGarcesO <jacobogarcesoquendo@gmail.com>
2022-07-28 18:04:21 -0500

    Refactor initialized

--- src/loaders/index.ts
+++ src/loaders/index.ts
@@ -1,11 +1,12 @@
 import { Application } from 'express';
 import { log } from '../utils/logs';
-import applicationLoader, { startServer } from './application';
+import Server from './server';
 import databaseLoader from './database';
 import routesLoader from './routes';
 
 export default async (app: Application) => {
-  await applicationLoader(app);
+  const server = new Server(app);
+  await server.init();
   await routesLoader(app);
-  databaseLoader.then(startServer.bind(this, app)).catch(log.bind(this));
+  databaseLoader.then(() => server.start()).catch(log.bind(this));
 };
